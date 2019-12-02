5bbb62419c8fcf600ddc0d45ae2c220c0959bc18
Dan Rogers <dan.rogers@idfive.com>
2019-12-02 13:00:06 -0700

    create new clone work

--- README.md
+++ README.md
@@ -6,37 +6,44 @@ This repo holds bash scripts and other files to help in the automation of Howard
 
 Clone this repo into your ~/Sites folder, as "_hal"
 
- - `$ cd ~/Sites`
- - `$ git clone https://https://github.com/howard-university-web-services/Howard-Automation-Library.git _hal`
+- `$ cd ~/Sites`
+- `$ git clone https://https://github.com/howard-university-web-services/Howard-Automation-Library.git _hal`
+
+### Add Local Config
+
+- `$ cd ~/Sites/_hal`
+- `$ cp hal_config.default.txt hal_config.txt`
+- Edit hal_config.txt to use your acquia credentials.
 
 ### Add and configure acquia CLI
 
- - `git clone https://github.com/typhonius/acquia_cli.git`
- - See [documentation](https://github.com/typhonius/acquia_cli) on adding API keys/etc locally.
- - Ensure Acquia CLI is installed and running correctly.
- 
+- `git clone https://github.com/typhonius/acquia_cli.git`
+- See [documentation](https://github.com/typhonius/acquia_cli) on adding API keys/etc locally.
+- Ensure Acquia CLI is installed and running correctly.
+
 ### Requirements
 
 Be sure the following are up and running correctly on your local machine:
 
- - [Acquia DevDesktop](https://www.acquia.com/drupal/acquia-dev-desktop)
- - [Acquia CLI](https://github.com/typhonius/acquia_cli)
- - [Drush](https://docs.drush.org/en/master/install/) -- Already present if using DevDesktop
+- [Acquia DevDesktop](https://www.acquia.com/drupal/acquia-dev-desktop)
+- [Acquia CLI](https://github.com/typhonius/acquia_cli)
+- [Drush](https://docs.drush.org/en/master/install/) -- Already present if using DevDesktop
 
 ## Usage
 
- - Ensure you are in the proper folder, especially for partials
- - `$ sh your_desired_script.sh`
- - Do not use sudo
+- Ensure you are in the proper folder, especially for partials
+- `$ sh your_desired_script.sh`
+- Do not use sudo
 
 ## Drupal 8
+
 The following scripts are available:
 
 ### Acquia
 
 #### Initial spinup of a multisite site, and clone dev.coasdept
 
- - This script creates a new multisite install locally, adjusts settings.php and sites.php with needed parameters, create a new multisite DB on acquia, clone the dev.coasdept.howard.edu DB and Files into it. 
- - You will need to keep a loose eye on the terminal to put in passwords/etc occasionally.
- - `$ cd ~/Sites/devdesktop/DESIRED_CODEBASE/docroot`
- - `$ sh ~/Sites/_hal/drupal/acquia/create_new_multisite.sh`
+- This script creates a new multisite install locally, adjusts settings.php and sites.php with needed parameters, create a new multisite DB on acquia, clone the dev.coasdept.howard.edu DB and Files into it.
+- You will need to keep a loose eye on the terminal to put in passwords/etc occasionally.
+- `$ cd ~/Sites/devdesktop/DESIRED_CODEBASE/docroot/sites`
+- `$ sh ~/Sites/_hal/drupal/acquia/create_new_multisite.sh`
--- drupal/acquia/create_new_multisite.sh
+++ drupal/acquia/create_new_multisite.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 #
-# This script creates a new multisite install locally, adjusts settings.php and sites.php with needed parameters, 
+# This script creates a new multisite install locally, adjusts settings.php and sites.php with needed parameters,
 # create a new multisite DB on acquia, clone the dev.coasdept.howard.edu DB and Files into it.
 #
 # $ sh ~/Sites/_ial/drupal/pantheon/create_new_pantheon_site.sh
+#
+# Notes:
+# - Be sure hal_config.txt is set up and working
+# - Refresh all drush aliases via dev desktop or similar
+# - Be sure you are on master branch, and that the env in question has DEV ENV set to MASTER.
+# - Be ure you run from ~/Sites/devdesktop/DESIRED_CODEBASE/docroot/sites
 #
 # Dependencies:
 # - Drush: https://www.drush.org/
 # - Acquia CLI: https://github.com/typhonius/acquia_cli
 #
 # Paramaters:
-# - Site Name | 'example.howard.edu'
-# - Database Name | 'example'
+# - HR Name | 'Example Site' | 'Example School'
+# - Site Name | 'example.howard.edu' | 'school.howard.edu'
+# - Database Name | 'example' | 'school'
+# - Acquia API Email | 'your.email@howard.edu' | read from hal_config.txt
+# - Acquia API Key | 'xxxxxxxxxxxxxxxxxxxxxxxx' | read from hal_config.txt
 #
 
 echo "This script will create the local sites folder, commit and push to acquia."
 echo "Be sure you are on the correct branch to deploy to DEV env."
+
+source ~/Sites/_hal/hal_config.txt
+
+# Database Name, use as $DATABASE_NAME
+echo "Enter the Human Readable Name. The Human Readable name of the site (e.g. Example Site):"
+read HR_NAME
+
+# Check database name is not empty
+if [ -z "$HR_NAME" ]; then
+  echo "The HR name cannot be empty!"
+  exit 2
+fi
 
 # Site Name, use as $SITE_NAME
 echo "Enter the Site Name. The folder that will be created in /sites. This should follow the URL pattern(e.g. example.howard.edu):"
@@ -36,9 +57,6 @@ if [ -z "$DATABASE_NAME" ]; then
   echo "The database name cannot be empty!"
   exit 2
 fi
-
-# Move to the sites folder
-# cd ~/Sites/devdesktop/docroot/sites
 
 #If this site folder already exists, remove it
 if [ -d "$SITE_NAME" ]; then
@@ -64,3 +82,17 @@ cd $site_name
 echo "Reconfiguring settings.php..."
 find . -type f -exec sed -i '' -e "s/SITENAME/${SITE_NAME}/g" {} \;
 find . -type f -exec sed -i '' -e "s/DATABASENAME/${DATABASE_NAME}/g" {} \;
+
+echo "commiting to git, and pushing..."
+git add .
+git commit -m 'Adding $HR_NAME ($SITE_NAME), via Howard Automation Library'
+git push origin master --force
+
+# Acquia notes:
+# drush @hud8.dev --uri=dev.coasdept.howard.edu sql-dump --result-file= > hal_coasdept_dump.sql
+# drush @academicdepartments.dev --uri=dev.$SITE_NAME sql-drop
+# drush @academicdepartments.dev --uri=dev.$SITE_NAME sql-cli < hal_coasdept_dump.sql
+# rm hal_coasdept_dump.sql
+# drush @academicdepartments.dev --uri=dev.coas.howard.edu config:set system.site name "${HR_NAME}"
+# scp -3 -r hud8.dev@staging-14271.prod.hosting.acquia.com:/mnt/files/hud8.dev/sites/coasdept.howard.edu/files academicdepartments.dev@staging-14271.prod.hosting.acquia.com:/mnt/files/academicdepartments.dev/sites/$SITE_NAME
+# drush @academicdepartments.dev --uri=dev.$SITE_NAME cr
