32ecc3740072fa44eb0c0964b356cb374371b2e1
Ben Collins <ben.collins@howard.edu>
2019-12-09 12:43:41 -0500

    added updates for the remaining Howard packages (https://packagist.org/packages/howard/)

--- drupal/acquia/update_hp_paragraphs.sh
+++ drupal/acquia/update_hp_paragraphs.sh
@@ -1,2 +1,7 @@
-#updates the Howard Paragraph Modules
+#updates all of the Howard Packages (https://packagist.org/packages/howard/)
 composer update 'howard/howard_paragraphs'
+composer update 'howard/howard_content_types'
+composer update 'howard/hu_general'
+composer update 'howard/howard_special_alerts_feed'
+composer update 'howard/howard_sidebar_menu_block'
+
