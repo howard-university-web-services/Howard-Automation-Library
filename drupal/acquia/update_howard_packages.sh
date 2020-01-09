9d2bf94cc4fcf589dd719b4a98df4a95c598a6bc
Dan Rogers <dan.rogers@idfive.com>
2020-01-09 15:44:40 -0700

    minor formatting

--- drupal/acquia/update_howard_packages.sh
+++ drupal/acquia/update_howard_packages.sh
@@ -10,7 +10,6 @@
 # - Be sure you are on master branch, and that the env in question has DEV ENV set to MASTER.
 #
 # Dependencies:
-# - Drush: https://www.drush.org/
 # - Git
 # - Composer
 #
@@ -18,20 +17,20 @@
 # - Local Howard D8 Folders | an array of absolute paths to folders on your local machine | read from hal_config.txt
 #
 
-echo "This script will update all howard packagist projects in all local folders specified in hal_config.txt, commit and push to acquia."
+echo "This script will update all Howard packagist projects in all local folders specified in hal_config.txt, commit and push to acquia."
 echo "Be sure you are on the correct branch to deploy to DEV env in all folders/repos, usually 'master'."
-echo "You will be committing and pushing code. Be sure this is what you want to do."
+echo "You will be committing and pushing code. Be sure this is really what you want to do."
 
 source ~/Sites/_hal/hal_config.txt
 
 for app in ${LOCAL_HOWARD_D8_FOLDERS[@]}; do
-    echo "Running update in $app"
-    cd $app
-    composer update 'howard/*'
-    echo "commiting to git, and pushing..."
-    git add .
-    git commit -m 'Updating all Howard Packagist themes and modules, via Howard Automation Library'
-    git push origin master --force
+  echo "Running update in $app"
+  cd $app
+  composer update 'howard/*'
+  echo "commiting to git, and pushing..."
+  git add .
+  git commit -m 'Updating all Howard Packagist themes and modules, via Howard Automation Library'
+  git push origin master --force
 done
 
 exit 0
